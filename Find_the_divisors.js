e8c66284aa38a6f7156f15652363ab946fbd146a
Hunter Long <hunterlong@admins-MacBook-Pro.local>
2021-05-04 11:48:16 -0500

    added previous finished code challenges

---
+++ Find_the_divisors.js
@@ -0,0 +1,14 @@
+function divisors(integer) {
+    let arr = [];
+    for(let i = 2; i < integer; i++) {
+      if(integer % i == 0) {
+        arr.push(i);
+      }
+    }
+    if(arr.length == 0) {
+      return prime = integer + " is prime"
+    } else {
+      return arr;
+    }
+    
+};
